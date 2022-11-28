d8ea07a5f3305d7bad45d3c572b74bfcd71bda07
danielburke <danielburke1206@gmail.com>
2022-11-28 15:41:53 -0500

    Initial Commit

---
+++ AttendanceTracker/AttendanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendanceTrackerApp.swift
+//  AttendanceTracker
+//
+//  Created by Daniel Burke on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct AttendanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
